5f19b2c4486df0b0c2ee66063261bf1f2c03a8de
dramsammy <dramsammy@stuy.edu>
2019-02-13 10:42:15 -0500

    initialized first array

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,9 +1,14 @@
 public class KnightBoard{
-  private int[][] KnightBoard;
+  private int[][] board;
   public KnightBoard(int startingRows, int startingCols){
     if (startingRows <= 0 || startingCols <= 0){
       throw new IllegalArgumentException();
     }
-    KnightBoard = new int[startingRows][startingCols];
+    board = new int[startingRows][startingCols];
+    for (int i = 0; i < startingRows; i++){
+      for (int a = 0; a < startingCols; a++){
+        board[i][a] = 0;
+      }
+    }
   }
 }
