f541675eecea32bdc52f49bc7fc2496d910e5453
dramsammy <dramsammy@stuy.edu>
2019-02-13 10:45:49 -0500

    added more instance variables

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,14 +1,26 @@
 public class KnightBoard{
   private int[][] board;
+  private int rows;
+  private int cols;
   public KnightBoard(int startingRows, int startingCols){
     if (startingRows <= 0 || startingCols <= 0){
       throw new IllegalArgumentException();
     }
+    rows = startingRows;
+    cols = startingCols;
     board = new int[startingRows][startingCols];
-    for (int i = 0; i < startingRows; i++){
-      for (int a = 0; a < startingCols; a++){
+    for (int i = 0; i < rows; i++){
+      for (int a = 0; a < cols; a++){
         board[i][a] = 0;
       }
+    }
+  }
+
+
+  public String toString(){
+    String returnValue = "";
+    for (int i = 0; i < rows; i++){
+      for
     }
   }
 }
